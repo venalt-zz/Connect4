a418b990e2c9dfd05651ab59aac794e19b76a993
unknown <rlsantos@uwaterloo.ca>
2014-11-02 23:55:40 -0500

    Finished! Need to test on other computers because my resolution is weird.

--- connect4.js
+++ connect4.js
@@ -1,95 +1,110 @@
 var Connect4Board = {
 	boardColor : "#33CCCC",
-	pieceColors : ["#FFF", "#FF6600", "#FF9933"],
+	pieceColors : ["#FFF", "#C72E00", "#FFEE00"],
 	board : [[],[],[],[],[],[],[]],
+	numPieces : [],
+	winState : 0,
 	context : {},
+	currentPlayer : 1,
 	init : function() {
-		Connect4Board.canvas = document.getElementById("gameBoard");
-		Connect4Board.context = Connect4Board.canvas.getContext("2d");
+		this.canvas = document.getElementById("gameBoard");
+		this.context = this.canvas.getContext("2d");
+		this.currentPlayer = 1;
+		this.winState = 0;
 
 		for (var i = 0; i < 7; i++){
 			for (var j = 0; j < 7; j++){
-				Connect4Board.board[i][j] = 0;
+				this.board[i][j] = 0;
 			}
+			this.numPieces[i] = 0;
 		}
 
-		var buttons = document.getElementById("buttons");
-		for (var i = 0; i < 7; i++){
-			var button = document.createElement("button");
-			var label = document.createTextNode(i);
-			button.appendChild(label);
+		document.getElementById("resetButton").addEventListener("click", this.init.bind(this),false);
 
-			button.addEventListener("click",(function(i){ 
-				return function(e){
-	   				Connect4Board.addPiece(i, 1);
-	   			}
-			})(i),false);
-			buttons.appendChild(button);
-		}
-
-		for (var i = 0; i < 7; i++){
-			var button = document.createElement("button");
-			var label = document.createTextNode(i);
-			button.appendChild(label);
-
-			button.addEventListener("click",(function(i){ 
-				return function(e){
-	   				Connect4Board.addPiece(i, 2);
-	   			}
-			})(i),false);
-			buttons.appendChild(button);
-		}
-
-		Connect4Board.update();
+		this.update();
 	}, 
 	addPiece : function (column, player) {
-		currentHeight = Connect4Board.board[column][6]; //7th value is a count
-		Connect4Board.board[column][5 - currentHeight] = player;
+		currentHeight = this.numPieces[column];
+		if (currentHeight == 6 || this.winState) return;
+		this.board[column][5 - currentHeight] = player;
 
-		Connect4Board.board[column][6]++;
-		Connect4Board.update();
-		if (Connect4Board.checkWin(player, column, 5 - currentHeight)) alert("YOU WON!!");
+		this.numPieces[column]++;
+		if (this.checkWin(player, column, 5 - currentHeight)){
+			this.update();
+			alert("Congratulations, you've won!");
+			this.winState = 1;
+		}
+		else {
+			this.currentPlayer = (1 + (this.currentPlayer == 1));
+			this.update();
+		}
 	}, 
 	update : function (){
-		Connect4Board.clear();
-		
-		Connect4Board.context.fillStyle = Connect4Board.boardColor;
-		Connect4Board.context.fillRect(0, 0, Connect4Board.canvas.width, Connect4Board.canvas.height);
+		this.clear();
+		document.getElementById("buttons").innerHTML = "";
 
-		var pieceDimension = Connect4Board.canvas.width / 7; 
+		this.drawButtons();
+		this.drawPieces();
+	},
+	drawButtons : function(){
+		var buttons = document.getElementById("buttons");
+
+		for (var i = 0; i < 7; i++){
+			var button = document.createElement("div");
+			button.className = "connect4-btn";
+
+			var circle = document.createElement("div");
+			circle.className = "connect4-circle";
+			circle.style.backgroundColor = this.pieceColors[this.currentPlayer];
+
+			button.appendChild(circle);
+
+			button.addEventListener("click",(function(i){ 
+				return function(e){
+	   				Connect4Board.addPiece(i, Connect4Board.currentPlayer);
+	   			}
+			})(i),false);
+			buttons.appendChild(button);
+		}
+	},
+	drawPieces : function(){
+		this.context.fillStyle = this.boardColor;
+		this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+		var pieceDimension = this.canvas.width / 7; 
 
 		for (var i = 0; i < 7; i++){
 			for (var j = 0; j < 6; j++){
-				pieceColor = Connect4Board.pieceColors[Connect4Board.board[i][j]];
-				Connect4Board.context.fillStyle = pieceColor;
+				pieceColor = this.pieceColors[this.board[i][j]];
+				this.context.fillStyle = pieceColor;
 
-				Connect4Board.context.beginPath();
-				Connect4Board.context.arc((i*pieceDimension)+pieceDimension/2, (j*pieceDimension)+pieceDimension/2, pieceDimension/3, 0, 2 * Math.PI, false);
-				Connect4Board.context.fill();
+				this.context.beginPath();
+				this.context.arc((i*pieceDimension)+pieceDimension/2, (j*pieceDimension)+pieceDimension/2, pieceDimension/3, 0, 2 * Math.PI, false);
+				this.context.fill();
 			}
 		}
 	},
 	clear : function(){
-		Connect4Board.context.clearRect(0, 0, 700, 700);
+		this.context.clearRect(0, 0, 700, 700);
 	}, 
 	checkWin : function (player, column, row) {
-		if (Connect4Board.inAColumn(player, column, row)){
+		if (this.inAColumn(player, column, row)){
 			return true;
 		}
-		else if (Connect4Board.board[3][6] > 0){ // Middle Column needs to have some pieces
-			if (Connect4Board.inARow(player, row)){
+		else if (this.numPieces[3] > 0){ // Middle Column needs to have some pieces
+			if (this.inARow(player, row)){
 				return true;
 			}
-			else if (Connect4Board.isDiagonal(player, column, row)) return true;
+			else if (this.isDiagonal(player, column, row)) return true;
 		}
 		
 		return false;
 	}, 
 	inAColumn : function (player, column, row){
-		if (Connect4Board.board[column][6] >= 4){ // Check if the column has at least 4 pieces
+		if (this.numPieces[column] >= 4){ // Check if the column has at least 4 pieces
 			var count = 0;
 
-			while (Connect4Board.board[column][row] == player){
+			while (this.board[column][row] == player){
 				row++;
 				count++;
 			}
@@ -102,7 +117,7 @@ var Connect4Board = {
 		var count = 0, column = 0;
 
 		while (column <= 6){
-			if (Connect4Board.board[column][row] == player){
+			if (this.board[column][row] == player){
 				count++;
 				if (count == 4) return true;
 			}
@@ -116,56 +131,56 @@ var Connect4Board = {
 		return false;
 	}, 
 	isDiagonal : function (player, column, row){
-		if ((row + Math.abs(column - 3) < 6) && Connect4Board.board[3][row + Math.abs(column - 3)] == player){ // Diagonal lower in middle column
+		if ((row + Math.abs(column - 3) < 6) && this.board[3][row + Math.abs(column - 3)] == player){ // Diagonal lower in middle column
 			if (column < 3){
-				while ((column > 0 && row > 0) && Connect4Board.board[column - 1][row - 1] == player){
+				while ((column > 0 && row > 0) && this.board[column - 1][row - 1] == player){
 					column--;
 					row--;
 				}
 
 				var count = 0;
-				while (Connect4Board.board[column][row] == player){
+				while (this.board[column][row] == player && (row < 6)){
 					count++;
 					column++;
 					row++;
 				}
 			}
 			else{
-				while ((column < 6 && row > 0) && Connect4Board.board[column + 1][row - 1] == player){
+				while ((column < 7 && row > 0) && this.board[column + 1][row - 1] == player){
 					column++;
 					row--;
 				}
 
 				var count = 0;
-				while (Connect4Board.board[column][row] == player){
+				while (this.board[column][row] == player && (row < 6)){
 					count++;
 					column--;
 					row++;
 				}
 			}
 		}
-		else if (Connect4Board.board[3][row - Math.abs(column - 3)] == player){ // Diagonal higher in middle column
+		else if (this.board[3][row - Math.abs(column - 3)] == player){ // Diagonal higher in middle column
 			if (column < 3){
-				while ((column > 0 && row < 5) && Connect4Board.board[column - 1][row + 1] == player){
+				while ((column > 0 && row < 6) && this.board[column - 1][row + 1] == player){
 					column--;
 					row++;
 				}
 
 				var count = 0;
-				while (Connect4Board.board[column][row] == player){
+				while (this.board[column][row] == player){
 					count++;
 					column++;
 					row--;
 				}
 			}
 			else{
-				while ((column < 6 && row < 5) && Connect4Board.board[column + 1][row + 1] == player){
+				while ((column < 7 && row < 6) && this.board[column + 1][row + 1] == player){
 					column++;
 					row++;
 				}
 
 				var count = 0;
-				while ((column > 0 && row > 0) && Connect4Board.board[col][row] == player){
+				while ((column > 0 && row > 0) && this.board[col][row] == player){
 					count++;
 					column--;
 					row--;
@@ -177,7 +192,7 @@ var Connect4Board = {
 	},
 	print : function (){
 		for (var i = 0; i < 7; i++){
-			console.log(Connect4Board.board[i]);
+			console.log(this.board[i]);
 		}
 	}
 }
